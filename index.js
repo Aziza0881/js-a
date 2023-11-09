b0586bcf5ea47b84247a754ed49a960268af2196
aziza <azizabonusaliyeva@gmail.com>
2023-11-09 14:32:43 +0500

    js-a
    js-a

---
+++ index.js
@@ -0,0 +1,13 @@
+let i=0
+while (i<=9) {
+    i++
+    document.write( `7*${i}= ${i*7} <br/>`)
+}
+
+let k = 0
+document.write(`<hr>`)
+do {
+    k++
+    document.write( `9*${k}= ${k*9} <br/>`)
+    
+} while (k<=9)
